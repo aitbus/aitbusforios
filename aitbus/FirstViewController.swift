250acc0f908eb8102f33c050e7629119017f13ba
ppengotsu <ppengotsu.renrakusaki@gmail.com>
2016-01-20 23:37:30 +0900

    プロジェクト作成 first commit make project

---
+++ aitbus/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  aitbus
+//
+//  Created by ppengotsu on 2016/01/20.
+//  Copyright © 2016年 ppengotsu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
